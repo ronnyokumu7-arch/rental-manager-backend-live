0461270123540279470abac5e59e2fb9af892fde
Ronny Okumu <ronnyokumu7@gmail.com>
2026-06-29 02:18:59 +0300

    feat: update start.sh script to start the application

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Running database migrations..."
-alembic upgrade head
+cd backend && alembic upgrade head
 echo "Starting application..."
 gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
