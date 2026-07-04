81650aa0ef2f4c26ffce9983981d4a7e9b8d27ac
Ronny Okumu <ronnyokumu7@gmail.com>
2026-07-04 17:32:31 +0300

    feat: Update start.sh to use 2 workers and set timeout to 120 seconds

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ alembic upgrade head
 
 # 2. Start the app using Gunicorn, binding to the correct host and port
 echo "Starting application..."
-exec gunicorn app.main:app -w 1 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
+exec gunicorn app.main:app -w 2 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT --timeout 120
